8388e6350ae502dd0c5db3aacc94bc1adf686823
Matthew Collingridge <MatthewCollingridge@users.noreply.github.com>
2015-10-22 14:32:31 +1100

    Update Run.sh
    added sleep delays, between calls, so enough time can pass for correct details.
    added a faster originator time for batmand and vis server at laptop, just encase we want it down the track

--- Run.sh
+++ Run.sh
@@ -5,9 +5,11 @@ echo "Setting up Edison Batmand mesh"
 
 echo ".... batmand ...."
 cd /
-./home/root/batmand/batmand wlan0
+./home/root/batmand/batmand -o 500 -s 10.42.0.1 wlan0
 
 echo ".... mesh ...."
+
+sleep 0.5
 
 wpa_cli -iwlan0 disconnect
 wpa_cli -iwlan0 remove_network all
@@ -23,9 +25,14 @@ wpa_cli -iwlan0 enable_network 0
 wpa_cli -iwlan0 status
 udhcpd wlan0
 
+sleep 0.5
+
 clear
 echo ".... Result ...."
 wpa_cli -iwlan0 status
+
+sleep 0.5
+
 ifconfig wlan0
 
 echo "done"
